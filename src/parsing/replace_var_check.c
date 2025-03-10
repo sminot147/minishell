fd8fc746adb23c4b853aaa96bc652afed0d69d29
Simeon Minot <sminot@z2r8p4.42lyon.fr>
2025-03-10 16:46:48 +0100

    patch again is_here_doc_name is replace var

--- src/parsing/replace_var_check.c
+++ src/parsing/replace_var_check.c
@@ -6,7 +6,7 @@
 /*   By: sminot <simeon.minot@outlook.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 19:22:27 by madelvin          #+#    #+#             */
-/*   Updated: 2025/03/08 16:44:26 by sminot           ###   ########.fr       */
+/*   Updated: 2025/03/10 16:40:38 by sminot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,22 @@
 t_bool	is_heredoc_name(char *input, size_t pos_var, t_alloc *all, \
 						t_bool is_in_herdoc)
 {
-	size_t		i;
-	size_t		pos_prev_token;
-	char		*value_prev_token;
+	size_t	tmp;
+	size_t	pos_act_token;
+	size_t	pos_prev_token;
+	char	*value_prev_token;
 
 	if (is_in_herdoc == TRUE || pos_var == 0)
 		return (FALSE);
-	i = 0;
+	pos_act_token = 0;
 	pos_prev_token = 0;
-	while (i < pos_var)
+	while (pos_prev_token < pos_var)
 	{
-		pos_prev_token = i;
-		i += size_to_move(&input[i]);
-		if (!input[i])
-		return (FALSE);
+		tmp = pos_act_token + size_to_move(&input[pos_act_token]);
+		if	(tmp > pos_var)
+			break ;
+		pos_prev_token = pos_act_token;
+		pos_act_token = tmp;
 	}
 	value_prev_token = extract_next_token(&input[pos_prev_token], all);
 	if (ft_strcmp(value_prev_token, "<<") == 0)
